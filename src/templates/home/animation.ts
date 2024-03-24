92a2123a32447598f87ee9c513d7c0011fe87884
João Damazio <joao.damazio@fiap.com.br>
2024-03-24 14:29:34 -0300

    🦁

--- src/templates/home/animation.ts
+++ src/templates/home/animation.ts
@@ -1,7 +1,6 @@
 import {useRef} from "react";
 import useRefElements from "../../hooks/useRefElements";
 import gsap from "gsap";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import {useGSAP} from "@gsap/react";
 import inverseMapRange from "../../utils/inverse-map-range";
 
@@ -11,57 +10,45 @@ export default () => {
   const [cardsRef, setCardRef] = useRefElements<HTMLElement>();
 
   useGSAP(() => {
-    const tl1 = stContainerSticky();
-    const tl2 = stCardsSlide();
-
     const mm = gsap.matchMedia();
     mm.add("(min-width: 789px)", () => {
-      ScrollTrigger.create({
-        trigger: pinRef.current,
-        start: "top 45%",
-        end: "+=1500 45%",
-        scrub: true,
-        pin: true,
-        onUpdate: ({ progress }) => {
-          tl1.progress(progress);
-          tl2.progress(progress);
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: pinRef.current,
+          start: "top 45%",
+          end: "+=1800 45%",
+          scrub: true,
+          pin: true,
         }
       });
 
+      const bounds = cardsRef.current.map(
+        (_, index) => ({min: index * 25, max: index * 95})
+      );
+
+      bounds.map((bound, index) => {
+        if (index === 0) {
+          return;
+        }
+
+        tl.to(cardsRef.current, {
+          y: (position) => {
+            const {min, max} = bounds[position];
+            const mapRange = inverseMapRange(0, position, min, max, index);
+            return `${mapRange}%`;
+          }
+        });
+
+        tl.to(containerRef.current, {yPercent: -bound.min}, '<');
+      });
+
       return () => {
-        tl1.revert();
-        tl2.revert();
+        gsap.set([cardsRef.current, containerRef.current], { clearProps: 'all' })
+
+        tl.kill();
       }
     });
   }, { scope: pinRef });
-
-  const stContainerSticky = () => {
-    const tl = gsap.timeline({ paused: true });
-
-    cardsRef.current.forEach((_, index) => {
-      if(index === 0) {
-        return;
-      }
-
-      tl.to(cardsRef.current, { y: (position) => {
-        const min = position * 25;
-        const max = position * 95;
-        const mapRange = inverseMapRange(0, position, min, max, index);
-
-        return `${gsap.utils.clamp(min, max, mapRange)}%`;
-      }});
-
-      tl.to(cardsRef.current, { yPercent: -index * 25}, '<');
-    });
-
-    return tl;
-  }
-
-  const stCardsSlide = () => {
-    const tl = gsap.timeline({ paused: true });
-
-    return tl;
-  }
 
   return {
     pinRef,
--- src/utils/inverse-map-range.ts
+++ src/utils/inverse-map-range.ts
@@ -2,8 +2,9 @@ const inverseMapRange = (inMin: number, inMax: number, outMin: number, outMax: n
   const ratio = (value - inMin) / (inMax - inMin);
 
   const res = outMin + (outMax - outMin) * (1 - ratio);
+  const clampRes = Math.min(Math.max(res, outMin), outMax);
 
-  return isNaN(res) ? 0 : res;
+  return isNaN(res) ? 0 : clampRes;
 }
 
 export default inverseMapRange;
